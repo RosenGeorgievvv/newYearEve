ee2dfa7b07e8beef52f8e5e12abfc2f4961d367d
Rosen Georgiev <73111903+RosenGeorgievvv@users.noreply.github.com>
2021-08-31 15:44:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('mins');
+const secondsElement = document.getElementById('secs');
+
+let newYears = '1 Jan 2022';
+
+function countDown() {
+    let newYearsDate = new Date(newYears);
+    let currentDate = new Date();
+
+    let allSeconds = (newYearsDate - currentDate) / 1000;
+    let days = Math.floor(allSeconds / 3600 / 24);
+    let hours = Math.floor(allSeconds / 3600) % 24;
+    let minutes = Math.floor(allSeconds / 60) % 60;
+    let seconds = Math.floor(allSeconds) % 60;
+
+    daysElement.textContent = days;
+    hoursElement.textContent = hours;
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+}
+countDown();
+
+setInterval(countDown, 1000);
